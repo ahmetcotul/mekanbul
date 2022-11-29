c2300d3718dcd3f973eba2ca0eea5c13ac2bdd74
ahmetcotul <ahmetcotul0@gmail.com>
2022-11-29 20:22:47 +0300

    odev6 checked

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mongoose = require("./app_api/models/db")
+require("./app_api/models/db")
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var apiRouter = require('./app_api/routes/index');
 
 var app = express();
-app.use('/api',apiRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
@@ -20,6 +19,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api',apiRouter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var dbURI = "mongodb+srv://ahmett:uykIknYCywJES8gw@cluster0.gmxz3hn.mongodb.net/?retryWrites=true&w=majority";
+//var dbURI = "mongodb+srv://ahmett:uykIknYCywJES8gw@cluster0.gmxz3hn.mongodb.net/?retryWrites=true&w=majority";
+var dbURI = "mongodb://127.0.0.1/mekanbul";
 mongoose.connect(dbURI);
 
 
